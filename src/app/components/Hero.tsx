63f2a00cb996e2ce96b70cd81fc0fa86154c9581
NaokiTM <ntsome21@gmail.com>
2024-10-22 23:57:32 +0100

    new google font imported into hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import { Libre_Baskerville } from 'next/font/google';
+import { librebaskerville } from '../layout'
 
 export default function Hero() {
-
-    const libreBaskerville = Libre_Baskerville({
-        subsets: ['latin'],
-        weight: ['400'],
-    })
 
   return (
     <div className='flex flex-col items-center h-[60vh] justify-center'>
         <div className='text-9xl font-medium tracking-tighter leading-none bg-white'>CalorieSync</div>
         <div>
-            <h2 className={`${libreBaskerville.className} italic text-3xl font-medium`}>Dynamic fitness data at your fingertips.</h2>
+            <h2 className={`${librebaskerville.className} italic text-3xl font-medium`}>Dynamic fitness data at your fingertips.</h2>
         </div>
     </div>
   )
