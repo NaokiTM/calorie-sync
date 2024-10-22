7509a6fdd32c46129e86580e8abfe14c684879e6
NaokiTM <ntsome21@gmail.com>
2024-10-23 00:41:26 +0100

    changed button to link in hero, created folder for caloriesync dashboard

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { instrumentserif } from '../layout'
 
 export default function Hero() {
@@ -7,7 +8,7 @@ export default function Hero() {
     <div className='flex flex-col items-center h-[60vh] justify-center'>
         <div className='text-9xl font-medium tracking-tighter leading-none bg-white'>CalorieSync</div>
         <h2 className={`${instrumentserif.className} italic text-5xl`}>Dynamic fitness data at your fingertips.</h2>
-        <button className='bg-blue-600 text-white p-2 mt-4 rounded-md text-lg'>launch CalorieSync</button>
+        <Link className='bg-blue-600 text-white p-2 mt-4 rounded-md text-lg' href=''>launch CalorieSync</Link>
     </div>
   )
 }
