f5cc52057fa429635306691edfa592db027f66f0
NaokiTM <ntsome21@gmail.com>
2024-10-22 22:23:57 +0100

    added geist sans to layout.tsx classname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${GeistSans.className} antialiased`}
       >
         {children}
       </body>
