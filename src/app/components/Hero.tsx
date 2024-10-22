2203e76053377501dd653660dcc9c1ebacf3b8f5
NaokiTM <ntsome21@gmail.com>
2024-10-23 00:35:51 +0100

    added logo to navbar (copied navbar from another one of my projects)

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,9 +6,8 @@ export default function Hero() {
   return (
     <div className='flex flex-col items-center h-[60vh] justify-center'>
         <div className='text-9xl font-medium tracking-tighter leading-none bg-white'>CalorieSync</div>
-        <div>
-            <h2 className={`${instrumentserif.className} italic text-5xl`}>Dynamic fitness data at your fingertips.</h2>
-        </div>
+        <h2 className={`${instrumentserif.className} italic text-5xl`}>Dynamic fitness data at your fingertips.</h2>
+        <button className='bg-blue-600 text-white p-2 mt-4 rounded-md text-lg'>launch CalorieSync</button>
     </div>
   )
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import logo from '../../../public/logo.svg'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+        <div className='flex justify-between p-2'>
+          <div className='space-x-6 flex items-center pl-2'>
+              <Link href = '/'>
+                <Image src={logo} alt="caloriesync logo" width={50} height={50} />
+              </Link>
+          </div>
+          <div className='space-x-6 flex items-center p-6'>
+              <Link href = '/login'>Log in</Link>
+              <Link href = '/dashboard'>Dashboard</Link>
+              <Link href = '/contact'>Contact</Link>
+          </div>
+        </div>
+      </>
   )
 }
