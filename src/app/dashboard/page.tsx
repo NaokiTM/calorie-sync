26942f88e977db6aaeb9ffa28708fe793a0a76b4
NaokiTM <ntsome21@gmail.com>
2024-10-23 01:09:35 +0100

    added rechart library using --legacy-peer-deps

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>your dashboard</div>
+    <>
+        <div>chart1 pie chart calories for the day </div>
+        <div>chart2 calories throughout the week</div>
+        <div>chart3 nutrition bar chart for the day with daily limit </div>
+        <div>chart4 workout consistency throughout the year (like the github one, but different colours for different split profiles</div>
+    </>
   )
 }
