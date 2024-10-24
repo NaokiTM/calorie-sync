3f88fd535b48a158dd4c8befbc017f46af8977ad
NaokiTM <ntsome21@gmail.com>
2024-10-24 16:48:19 +0100

    removed pie chart to be replaced by circular progress bar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Metadata } from 'next';
-import Caloriepie from '../components/dash/Caloriepie';
+import CalorieProgress from '../components/dash/Caloriepie';
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -11,7 +11,7 @@ export default function page() {
   return (
     <>
       <div className='h-[100vh]'>
-          <Caloriepie />
+          <CalorieProgress />
           <div>chart1 pie chart calories for the day </div>
           <div>chart2 calories throughout the week</div>
           <div>chart3 nutrition bar chart for the day with daily limit </div>
