9cd65f0b1dd009fb22d5b518cd41edcf68aa4f85
NaokiTM <ntsome21@gmail.com>
2024-10-25 17:22:29 +0100

    corrected typos

--- src/app/components/dash/CalorieProgress.tsx
+++ src/app/components/dash/CalorieProgress.tsx
@@ -18,11 +18,11 @@ export default function CalorieProgress() {
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300px" height="300px">
          <defs>
             <linearGradient id="GradientColor">
-               <stop offset="0%" stop-color="#e91e63" />
-               <stop offset="100%" stop-color="#673ab7" />
+               <stop offset="0%" stopColor="#e91e63" />
+               <stop offset="100%" stopColor="#673ab7" />
             </linearGradient>
          </defs>
-         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+         <circle cx="80" cy="80" r="70" strokeLinecap="round" />
       </svg>
     </>
   )
